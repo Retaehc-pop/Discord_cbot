078281f156e26555147f7e6943f920e7da5465d0
Retaehc-pop <papop2003@gmail.com>
2023-03-09 09:58:15 +0100

    .

---
+++ exceptions/__init__.py
@@ -0,0 +1,38 @@
+from discord.ext import commands
+
+
+class UserBlacklisted(commands.CheckFailure):
+    """
+    Thrown when a user is attempting something, but is blacklisted.
+    """
+
+    def __init__(self, message="User is blacklisted!"):
+        self.message = message
+        super().__init__(self.message)
+
+
+class UserNotOwner(commands.CheckFailure):
+    """
+    Thrown when a user is attempting something, but is not an owner of the bot.
+    """
+
+    def __init__(self, message="User is not an owner of the bot!"):
+        self.message = message
+        super().__init__(self.message)
+
+class VoiceError(commands.CheckFailure):
+    """
+    thrown when there is an error with voice
+    """
+    def __init__(self, message="VOICE Error!"):
+        self.message = message
+        super().__init__(self.message)
+
+
+class YTDLError(commands.CheckFailure):
+    """
+    thrown when there is an error with youtube-dl
+    """
+    def __init__(self, message="YTDL Error!"):
+        self.message = message
+        super().__init__(self.message)
